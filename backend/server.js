cb0663c9a1be0f86ab2d0937fdb822fd59f9177a
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-06 16:04:56 +0300

    Setup middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const { errorHandler, notFound } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
 
+// port
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(fileUpload({ useTempFiles: true }));
+
+//Error middlewares
+app.use(errorHandler);
+app.use(notFound);
+
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`Connect to mongoDB`);
   } catch (err) {
     console.log(`Error: ${err.message}`);
   }
 };
 
-app.listen(process.env.PORT, () => {
-  console.log("Server listening on 8000");
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
   connect();
 });
