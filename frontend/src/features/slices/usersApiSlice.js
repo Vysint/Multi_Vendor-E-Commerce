8bb1efa4365bc2b4863822beacb3f05935ad08f0
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-07 14:57:37 +0300

    Added Redux authSlice and apiSlice

---
+++ frontend/src/features/slices/usersApiSlice.js
@@ -0,0 +1,31 @@
+import { apiSlice } from "./apiSlice";
+
+const USERS_URL = "/api/v2/users";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/login`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    signUp: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/register`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useSignUpMutation, useLogoutMutation } =
+  usersApiSlice;
