ebac5f66049e8a76f77266a20822f22e39795777
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-11 02:29:21 +0300

    User signup route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, login } = require("../controllers/userController");
+const { fileUpload } = require("../utils/fileUpload");
 
-router.post("/register", registerUser);
+router.post("/register", fileUpload.single("file"), registerUser);
 router.post("/login", login);
-
 
 module.exports = router;
