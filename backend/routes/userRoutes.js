535aa125122d984059f9e624b970e3241552066e
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-12 02:35:03 +0300

    Forgot Pasword route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ const {
   login,
   logout,
   getUserProfile,
+  forgotPassword,
 } = require("../controllers/userController");
 const { fileUpload } = require("../utils/fileUpload");
 const protect = require("../middlewares/authMiddleware");
@@ -13,5 +14,6 @@ router.post("/register", fileUpload.single("file"), registerUser);
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/profile", protect, getUserProfile);
+router.post("/forgotpassword", forgotPassword);
 
 module.exports = router;
