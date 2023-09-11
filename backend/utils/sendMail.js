979937c711b708eedcdede26d37e47ac8f9d230c
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-12 01:10:50 +0300

    Update sendMail utility function

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -2,21 +2,31 @@ const nodemailer = require("nodemailer");
 
 const sendMail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.SMPT_HOST,
-    port: process.env.SMPT_PORT,
-    service: process.env.SMPT_SERVICE,
+    host: process.env.EMAIL_HOST,
+    port: 587,
     auth: {
-      user: process.env.SMPT_MAIL,
-      pass: process.env.SMPT_PASSWORD,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
   const mailOptions = {
     from: process.env.SMPT_MAIL,
     to: options.mail,
     subject: options.subject,
+    replyTo: options.replyTo,
     text: options.message,
   };
-  await transporter.sendMail(mailOptions);
+  // Send the mail
+  await transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
 };
 
 module.exports = sendMail;
