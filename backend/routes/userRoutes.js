72a0f9a89b7210d6fd418772352f726d9f3e880b
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-12 00:33:30 +0300

    Added logout and getuserprofile routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, login } = require("../controllers/userController");
+const {
+  registerUser,
+  login,
+  logout,
+  getUserProfile,
+} = require("../controllers/userController");
 const { fileUpload } = require("../utils/fileUpload");
+const protect = require("../middlewares/authMiddleware");
 
 router.post("/register", fileUpload.single("file"), registerUser);
 router.post("/login", login);
+router.post("/logout", logout);
+router.get("/profile", protect, getUserProfile);
 
 module.exports = router;
