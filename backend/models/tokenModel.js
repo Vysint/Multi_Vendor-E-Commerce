47e81f8d12f756ff0b39786e13c9b6769e9cc03d
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-12 02:34:24 +0300

    Corrected naming eror in token model schema

--- backend/models/tokenModel.js
+++ backend/models/tokenModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const tokenSchema = mongoose.Schema({
-  userid: {
+  userId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "user",
