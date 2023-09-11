ece393123b184500888cd5f389b7ff9e8b4096f6
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-11 15:34:15 +0300

    Updated home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
-
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  console.log(userInfo);
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h2>{userInfo.name}</h2>
+      <img src={userInfo.imageURL} alt="" style={{width:'300px', marginLeft:"40%"}}/>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
