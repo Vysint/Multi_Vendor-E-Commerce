ff19beb7568ae7c0fc84c94fdef252b669f6cebe
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-12 00:32:40 +0300

    Handled user state to keep user signed in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
   return (
     <>
       <Outlet />
