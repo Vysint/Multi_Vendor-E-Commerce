86f2c531554116dd7612ee36969742f88c099db2
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-12 03:51:55 +0300

    Reset Password route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,7 @@ const {
   logout,
   getUserProfile,
   forgotPassword,
+  resetPassword,
 } = require("../controllers/userController");
 const { fileUpload } = require("../utils/fileUpload");
 const protect = require("../middlewares/authMiddleware");
@@ -15,5 +16,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.get("/profile", protect, getUserProfile);
 router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resetToken", resetPassword);
 
 module.exports = router;
