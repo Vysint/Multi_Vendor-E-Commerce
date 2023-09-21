ae21feb17ec7dc08bac8aff70548d173f8c35280
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-21 15:30:16 +0300

    Added categories to home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import Categories from "../../components/categories/Categories";
 import Hero from "../../components/hero/Hero";
 
 const Home = () => {
   return (
     <div className="home">
       <Hero />
+      <Categories />
     </div>
   );
 };
