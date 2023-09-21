61fbca14acd959bc1204d3a3e210660760b0515f
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-21 22:51:15 +0300

    Added best deals section to the home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
-import Categories from "../../components/categories/Categories";
-import Hero from "../../components/hero/Hero";
+import BestDeals from "../../components/utils/bestDeals/BestDeals";
+import Categories from "../../components/utils/categories/Categories";
+import Hero from "../../components/utils/hero/Hero";
 
 const Home = () => {
   return (
     <div className="home">
       <Hero />
       <Categories />
+      <BestDeals />
     </div>
   );
 };
