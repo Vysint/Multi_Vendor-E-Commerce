5ae493a769b482f4f1e10e3690b2c7251dc699a5
Vincent Kipngetich <vinniebluz@icloud.com>
2023-09-29 01:50:34 +0300

    Added Events section to the home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import BestDeals from "../../components/utils/bestDeals/BestDeals";
 import Categories from "../../components/utils/categories/Categories";
+import Events from "../../components/utils/events/Events";
 import FeaturedProduct from "../../components/utils/featuredProduct/FeaturedProduct";
 import Hero from "../../components/utils/hero/Hero";
 
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Categories />
       <BestDeals />
+      <Events />
       <FeaturedProduct />
     </div>
   );
