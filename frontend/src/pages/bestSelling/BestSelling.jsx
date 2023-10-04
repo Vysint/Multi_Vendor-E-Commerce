9baeb3243905f27ec0f2b714762d4f3130bdd676
Vincent Kipngetich <vinniebluz@icloud.com>
2023-10-05 02:49:50 +0300

    Removed CSS file

---
+++ frontend/src/pages/bestSelling/BestSelling.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { productData } from "../../static/data";
+import ProductCard from "../../components/utils/card/ProductCard";
+import Header from "../../components/layout/Header";
+
+const BestSelling = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const d =
+      productData && productData.sort((a, b) => b.total_sell - a.total_sell);
+    setData(d);
+  }, []);
+
+  return (
+    <div>
+      <Header activeHeading={2} />
+      <div className="products_container">
+        <div className="products">
+          {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BestSelling;
