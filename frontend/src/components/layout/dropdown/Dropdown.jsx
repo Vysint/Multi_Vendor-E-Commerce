ee1d9de5f6b3c250e3e6a4dff8b3f0a7b444c9e7
Vincent Kipngetich <vinniebluz@icloud.com>
2023-10-20 13:21:22 +0300

    Code cleanup

--- frontend/src/components/layout/dropdown/Dropdown.jsx
+++ frontend/src/components/layout/dropdown/Dropdown.jsx
@@ -7,32 +7,30 @@ const Dropdown = ({ categories, setDropdown }) => {
   const submitHandler = (category) => {
     navigate(`/products?category=${category.title}`);
     setDropdown(false);
-    // You can remove the window.location.reload() line if not needed.
   };
 
   return (
     <div className="dropdown_section">
-      {categories &&
-        categories.map((category, index) => (
-          <div
-            className="dropdown_items"
-            key={index}
-            onClick={() => submitHandler(category)}
-          >
-            <img
-              src={category.image_Url}
-              alt=""
-              style={{
-                width: "25px",
-                height: "25px",
-                objectFit: "contain",
-                marginLeft: "10px",
-                userSelect: "none",
-              }}
-            />
-            <h3>{category.title}</h3>
-          </div>
-        ))}
+      {categories.map((category, index) => (
+        <div
+          className="dropdown_items"
+          key={index}
+          onClick={() => submitHandler(category)}
+        >
+          <img
+            src={category.image_Url}
+            alt=""
+            style={{
+              width: "25px",
+              height: "25px",
+              objectFit: "contain",
+              marginLeft: "10px",
+              userSelect: "none",
+            }}
+          />
+          <h3>{category.title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
