f4a8f77101e6ed524165eac4e4783990bb3d7dee
Vincent Kipngetich <vinniebluz@icloud.com>
2023-11-15 02:13:44 +0300

    Added Redux toolkit shopSlice action creators

---
+++ frontend/src/features/api/shopApiSlice.js
@@ -0,0 +1,18 @@
+import { apiSlice } from "./apiSlice";
+
+const SHOP_URL = "/api/v2/shop";
+
+export const shopApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    registerShop: builder.mutation({
+      query: (data) => ({
+        url: `${SHOP_URL}/register`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Shop"],
+    }),
+  }),
+});
+
+export const { useRegisterShopMutation } = shopApiSlice;
