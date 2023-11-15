b7e06a9e1a85faf67d3ad87a54ee212ec969d3fd
Vincent Kipngetich <vinniebluz@icloud.com>
2023-11-15 03:31:31 +0300

    Added seller login api post request route

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { registerShop } = require("../controllers/shopController");
+const { registerShop, shopLogin } = require("../controllers/shopController");
 const { fileUpload } = require("../utils/fileUpload");
 
 const router = express.Router();
 
 router.post("/register", fileUpload.single("file"), registerShop);
+
+router.post("/login", shopLogin);
 
 module.exports = router;
