c60b7c3166e3c0644ef587f0599cefce92d67ea7
Vincent Kipngetich <vinniebluz@icloud.com>
2023-11-24 14:14:46 +0300

    Added Shop Page sections

--- frontend/src/pages/shop/ShopHomePage.jsx
+++ frontend/src/pages/shop/ShopHomePage.jsx
@@ -1,9 +1,20 @@
-
+import ShopInfo from "../../components/shop/layout/shopInfo/ShopInfo";
+import ShopProfileData from "../../components/shop/layout/shopProfileData/ShopProfileData";
+import "./ShopHomePage.scss";
 
 const ShopHomePage = () => {
   return (
-    <div>ShopHomePage</div>
-  )
-}
+    <div className="shop_home_page_container">
+      <div className="home_page_container">
+        <div className="home_page_info">
+          <ShopInfo isOwner={true} />
+        </div>
+        <div className="shop_profile_data">
+          <ShopProfileData isOwner={true} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ShopHomePage
+export default ShopHomePage;
