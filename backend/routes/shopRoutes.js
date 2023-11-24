31b07c3791d3530caa455ce482c3e7d16a5198e5
Vincent Kipngetich <vinniebluz@icloud.com>
2023-11-24 14:15:55 +0300

    LoginStatus Route

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -3,6 +3,7 @@ const {
   registerShop,
   shopLogin,
   getSeller,
+  loginStatus,
 } = require("../controllers/shopController");
 const { fileUpload } = require("../utils/fileUpload");
 const protectShop = require("../middlewares/shopMiddleware");
@@ -10,9 +11,8 @@ const protectShop = require("../middlewares/shopMiddleware");
 const router = express.Router();
 
 router.post("/register", fileUpload.single("file"), registerShop);
-
 router.post("/login", shopLogin);
-
+router.get("/loggedin", protectShop, loginStatus);
 router.get("/:id", protectShop, getSeller);
 
 module.exports = router;
