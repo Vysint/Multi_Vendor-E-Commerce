0f9b3c115ce05c72bcf0332373475083a69033ad
Vincent Kipngetich <vinniebluz@icloud.com>
2023-11-28 03:00:06 +0300

    Added user redirection depending on user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useGetLoginStatusQuery } from "./features/api/shopApiSlice";
+import { setLogin } from "./features/slices/shopSlice";
 const App = () => {
   const { userInfo } = useSelector((state) => state.auth);
+  const { data } = useGetLoginStatusQuery();
+  const dispatch = useDispatch();
+
   const navigate = useNavigate();
   useEffect(() => {
     if (userInfo) {
       navigate("/");
     }
   }, [navigate, userInfo]);
+
+  useEffect(() => {
+    async function loginStatus() {
+      const status = data;
+      dispatch(setLogin(status));
+    }
+    loginStatus();
+  }, [data, dispatch]);
+
   return (
     <>
       <Outlet />
