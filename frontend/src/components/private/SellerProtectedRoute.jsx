5dd308b7e87ab70c56cca2b0a442903436228748
Vincent Kipngetich <vinniebluz@icloud.com>
2023-11-28 03:01:01 +0300

    Shop private routes file

--- frontend/src/components/private/SellerProtectedRoute.jsx
+++ frontend/src/components/private/SellerProtectedRoute.jsx
@@ -3,7 +3,10 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const SellerProtectedRoute = () => {
   const { isSeller } = useSelector((state) => state.shop);
+  // const location = useLocation();
   return isSeller ? <Outlet /> : <Navigate to="/shop-login" replace />;
 };
 
 export default SellerProtectedRoute;
+
+// state={{ from: location }}
