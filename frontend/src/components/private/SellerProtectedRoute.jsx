caa9d42609518cd3410a1efd57b99cb7dd146360
Vincent Kipngetich <vinniebluz@icloud.com>
2023-11-30 01:49:48 +0300

    Seller private route

--- frontend/src/components/private/SellerProtectedRoute.jsx
+++ frontend/src/components/private/SellerProtectedRoute.jsx
@@ -2,11 +2,11 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const SellerProtectedRoute = () => {
-  const { isSeller } = useSelector((state) => state.shop);
-  // const location = useLocation();
-  return isSeller ? <Outlet /> : <Navigate to="/shop-login" replace />;
+  const { seller } = useSelector((state) => state.shop);
+  
+  return seller ? <Outlet /> : <Navigate to="/shop-login" replace />;
 };
 
 export default SellerProtectedRoute;
 
-// state={{ from: location }}
+
