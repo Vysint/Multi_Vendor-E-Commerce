0e27eb505e55c67a0f38dac020dfcc179be9d66c
Vincent Kipngetich <vinniebluz@icloud.com>
2023-12-02 03:13:12 +0300

    Redux toolkit store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,28 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/slices/authSlice";
 import shopReducer from "./features/slices/shopSlice";
 import { apiSlice } from "./features/api/apiSlice";
-import { combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 
-const rootReducer = combineReducers({
-  auth: authReducer,
-  shop: shopReducer,
-  [apiSlice.reducerPath]: apiSlice.reducer,
-});
-
-const persistConfig = {
-  key: rootReducer,
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore({
-  reducer: persistedReducer,
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    shop: shopReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
-export const persistor = persistStore(store);
+export default store;
