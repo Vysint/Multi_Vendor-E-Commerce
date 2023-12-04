929363ea5d84129cfbd18f4b2adf2d0052a486ee
Vincent Kipngetich <vinniebluz@icloud.com>
2023-12-04 23:43:51 +0300

    Added the URL location to the seller  private route

--- frontend/src/components/private/SellerProtectedRoute.jsx
+++ frontend/src/components/private/SellerProtectedRoute.jsx
@@ -1,10 +1,15 @@
 import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const SellerProtectedRoute = () => {
   const { shopInfo } = useSelector((state) => state.shop);
+  const location = useLocation();
 
-  return shopInfo ? <Outlet /> : <Navigate to="/shop-login" replace />;
+  return shopInfo ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/shop-login" state={{ from: location }} replace />
+  );
 };
 
 export default SellerProtectedRoute;
