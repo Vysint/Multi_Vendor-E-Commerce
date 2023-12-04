fbc9ed689d3dca61a1483e3effd8ec92aae86bdd
Vincent Kipngetich <vinniebluz@icloud.com>
2023-12-04 23:30:36 +0300

    Added the URL location to the login private route

--- frontend/src/components/private/PrivateRoute.jsx
+++ frontend/src/components/private/PrivateRoute.jsx
@@ -1,10 +1,15 @@
 import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
+  const location = useLocation();
 
-  return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
+  return userInfo ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 };
 
 export default PrivateRoute;
