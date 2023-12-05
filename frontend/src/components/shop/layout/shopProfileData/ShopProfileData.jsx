d8fea5e6f586ef655bb59ca3baf73b39a6d6bc52
Vincent Kipngetich <vinniebluz@icloud.com>
2023-12-05 14:53:42 +0300

    Dynamically displayed the seller products

--- frontend/src/components/shop/layout/shopProfileData/ShopProfileData.jsx
+++ frontend/src/components/shop/layout/shopProfileData/ShopProfileData.jsx
@@ -1,9 +1,46 @@
-import './ShopProfileData.scss'
+import { useState } from "react";
+import { productData } from "../../../../static/data";
+import "./ShopProfileData.scss";
+import ProductCard from "../../../utils/card/ProductCard";
+import { Link } from "react-router-dom";
 
-const ShopProfileData = () => {
+const ShopProfileData = ({ isOwner }) => {
+  const [active, setActive] = useState(1);
   return (
-    <div>ShopProfileData</div>
-  )
-}
+    <div className="shop_profile_container">
+      <div>
+        <div className="shop_profile_items">
+          <div className="profile_items" onClick={() => setActive(1)}>
+            <h5 className={`${active === 1 ? "colored_text" : ""}`}>
+              Shop Products
+            </h5>
+          </div>
+          <div className="profile_items" onClick={() => setActive(2)}>
+            <h5 className={`${active === 2 ? "colored_text" : ""}`}>
+              Running Events
+            </h5>
+          </div>
+          <div className="profile_items" onClick={() => setActive(3)}>
+            <h5 className={`${active === 3 ? "colored_text" : ""}`}>
+              Shop Reviews
+            </h5>
+          </div>
+        </div>
 
-export default ShopProfileData
+        {isOwner && (
+          <Link to="/dashboard" className="--btn">
+            <span>Go Dashboard</span>
+          </Link>
+        )}
+      </div>
+      <br />
+      <div className="product_data_container">
+        {productData.map((i, index) => (
+          <ProductCard key={index} data={i} isShop={true} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShopProfileData;
