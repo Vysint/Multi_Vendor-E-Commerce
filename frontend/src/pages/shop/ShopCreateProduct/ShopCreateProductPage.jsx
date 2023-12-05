4cdc6591726e645b989efc85ce6e4f52e4d8419e
Vincent Kipngetich <vinniebluz@icloud.com>
2023-12-06 01:45:24 +0300

    Create product page

---
+++ frontend/src/pages/shop/ShopCreateProduct/ShopCreateProductPage.jsx
@@ -0,0 +1,13 @@
+import CreateProduct from "../../../components/shop/layout/createProduct/CreateProduct";
+import DashboardHeader from "../../../components/shop/layout/dashboardHeader/DashboardHeader";
+
+const ShopCreateProductPage = () => {
+  return (
+    <>
+      <DashboardHeader />
+      <CreateProduct />
+    </>
+  );
+};
+
+export default ShopCreateProductPage;
