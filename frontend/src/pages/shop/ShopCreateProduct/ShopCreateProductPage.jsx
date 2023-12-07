bbd5cae64b0959ffc57fe6da24b25996dfc3d2ad
Vincent Kipngetich <vinniebluz@icloud.com>
2023-12-07 12:10:17 +0300

    Seller/shop creation page

--- frontend/src/pages/shop/ShopCreateProduct/ShopCreateProductPage.jsx
+++ frontend/src/pages/shop/ShopCreateProduct/ShopCreateProductPage.jsx
@@ -1,11 +1,20 @@
 import CreateProduct from "../../../components/shop/layout/createProduct/CreateProduct";
 import DashboardHeader from "../../../components/shop/layout/dashboardHeader/DashboardHeader";
+import DashboardSidebar from "../../../components/shop/layout/dashboardSidebar/DashboardSidebar";
+import "./ShopCreateProductPage.scss";
 
 const ShopCreateProductPage = () => {
   return (
     <>
       <DashboardHeader />
-      <CreateProduct />
+      <div className="shop_create_product_page">
+        <div className="shop_create_product_sidebar">
+          <DashboardSidebar active={4} />
+        </div>
+        <div className="shop_create_product">
+          <CreateProduct />
+        </div>
+      </div>
     </>
   );
 };
