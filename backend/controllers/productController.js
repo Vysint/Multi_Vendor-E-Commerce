bc97f99cece17bf92ec5aa5598c9c61e06af6f93
Vincent Kipngetich <vinniebluz@icloud.com>
2024-01-17 03:29:21 +0300

    Product controller function

---
+++ backend/controllers/productController.js
@@ -0,0 +1,43 @@
+const Product = require("../models/productModel");
+
+// @desc   Create a product
+// route   POST /api/v2/products/create-product
+// @access Private
+exports.createProduct = async (req, res, next) => {
+  const {
+    name,
+    description,
+    category,
+    tags,
+    originalPrice,
+    discountPrice,
+    quantity,
+  } = req.body;
+
+  // Validation
+  try {
+    if (
+      !name ||
+      !description ||
+      !category ||
+      !tags ||
+      !originalPrice ||
+      !discountPrice ||
+      !quantity
+    ) {
+      res.status(400);
+      throw new Error("Please fill in all the required fields");
+    }
+  } catch (err) {
+    return next(err);
+  }
+
+  // Create a product
+  try {
+    const product = await Product.create({
+      
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
