6dcc3ae3bea03c223542c8304134cba98a0a7049
Vincent Kipngetich <vinniebluz@icloud.com>
2024-01-21 20:20:47 +0300

    Removed products reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/slices/authSlice";
 import shopReducer from "./features/slices/shopSlice";
-import productReducer from "./features/slices/productSlice";
 import { apiSlice } from "./features/api/apiSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     shop: shopReducer,
-    products: productReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
