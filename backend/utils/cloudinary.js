ee161c09691e241a01444b9ac35f9e84d2a92543
Vincent Kipngetich <vinniebluz@icloud.com>
2024-01-28 15:31:19 +0300

    Added a utility function for image deletion in cloudinary

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -6,4 +6,16 @@ cloudinary.config({
   api_secret: process.env.CLOUD_KEY_SECRET,
 });
 
-module.exports = cloudinary;
+// Utility function to delete an image by it's public_id;
+const deleteImage = async (public_id) => {
+  try {
+    const result = await cloudinary.uploader.destroy(public_id);
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+module.exports = deleteImage;
