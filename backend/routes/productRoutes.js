075ede20a6b97178122b2a346c471f468b63f1de
Vincent Kipngetich <vinniebluz@icloud.com>
2024-01-30 03:57:49 +0300

    Delete Product route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const {
   createProduct,
   getProducts,
+  deleteProduct,
 } = require("../controllers/productController");
 const protectShop = require("../middlewares/shopMiddleware");
 
 router.get("/", protectShop, getProducts);
 router.post("/create-product", protectShop, createProduct);
+
+router.delete("/:id", protectShop, deleteProduct);
 module.exports = router;
