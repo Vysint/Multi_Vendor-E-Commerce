1a262a8658efc99071fb1aba13f7bdd2813931ee
Vincent Kipngetich <vinniebluz@icloud.com>
2024-01-30 03:57:11 +0300

    Utility function to delete images in cloudinary

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,21 +1,20 @@
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_KEY,
-  api_secret: process.env.CLOUD_KEY_SECRET,
-});
-
 // Utility function to delete an image by it's public_id;
-const deleteImage = async (public_id) => {
-  try {
-    const result = await cloudinary.uploader.destroy(public_id);
-    console.log(result);
-    return result;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-};
+async function deleteImageFromCloudinary(publicId) {
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_KEY_SECRET,
+  });
 
-module.exports = deleteImage;
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (err) {
+    // Handle error
+    console.error("Error deleting image from cloudinary:", err);
+    throw new Error("Failed to delete image from cloudinary");
+  }
+}
+
+module.exports = { deleteImageFromCloudinary };
