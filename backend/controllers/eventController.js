d26ec48174b2de39182f856d2090942e332cebb3
Vincent Kipngetich <vinniebluz@icloud.com>
2024-01-31 15:16:21 +0300

    Events controller - Create event controller function

---
+++ backend/controllers/eventController.js
@@ -0,0 +1,59 @@
+const { request } = require("express");
+const Event = require("../models/eventModel");
+
+// @desc   Create an event
+// route   POST /api/v2/events/create-event
+// @access Private
+
+exports.createEvent = async (req, res, next) => {
+  const {
+    name,
+    description,
+    category,
+    start_date,
+    end_date,
+    originalPrice,
+    discountPrice,
+    stock,
+  } = req.body;
+
+  try {
+    if (
+      !name ||
+      !description ||
+      !category ||
+      !start_date ||
+      !end_date ||
+      !originalPrice ||
+      !discountPrice ||
+      !stock
+    ) {
+      res.status(500).json("Please fill all the required fields");
+    }
+  } catch (err) {
+    return next(err);
+  }
+
+  // Create an event
+  try {
+    const event = await Event.create({
+      name,
+      description,
+      category,
+      start_date,
+      end_date,
+      originalPrice,
+      discountPrice,
+      stock,
+      shop: req.seller._id,
+      images: req.body.images,
+    });
+    if (event) {
+      res.status(201).json(event);
+    } else {
+      throw new Error("Failed to create an event, please try again");
+    }
+  } catch (err) {
+    return next(err);
+  }
+};
