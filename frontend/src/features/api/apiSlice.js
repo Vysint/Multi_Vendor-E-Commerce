7618b483b3d5cc15937e1bb585d1f359d43886fe
Vincent Kipngetich <vinniebluz@icloud.com>
2024-02-07 16:51:17 +0300

    Added events tag

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -7,6 +7,6 @@ const baseQuery = fetchBaseQuery({
 
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ["User", "Shop", "Product"],
+  tagTypes: ["User", "Shop", "Product", "Event"],
   endpoints: () => ({}),
 });
