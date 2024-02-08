fa0cfdce890b3787e480454b79eddf3c4d77db9a
Vincent Kipngetich <vinniebluz@icloud.com>
2024-02-08 22:07:14 +0300

    Get events for a specific shop/seller route

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const { createEvent } = require("../controllers/eventController");
+const { createEvent, getEvents } = require("../controllers/eventController");
 const protectShop = require("../middlewares/shopMiddleware");
 
 router.post("/create-event", protectShop, createEvent);
+
+router.get("/", protectShop, getEvents);
 
 module.exports = router;
