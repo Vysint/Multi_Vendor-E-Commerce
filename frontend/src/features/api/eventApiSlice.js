bf57049e174ea2bb7ac2abc41cc195b50a69bac2
Vincent Kipngetich <vinniebluz@icloud.com>
2024-02-08 23:18:04 +0300

    Added delete event api request

--- frontend/src/features/api/eventApiSlice.js
+++ frontend/src/features/api/eventApiSlice.js
@@ -12,7 +12,21 @@ export const eventApiSlice = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ["Event"],
     }),
+    getEvents: builder.query({
+      query: () => `${EVENTS_URL}`,
+      providesTags: ["Event"],
+    }),
+    deleteEvent: builder.mutation({
+      query: (id) => ({
+        url: `${EVENTS_URL}/${id}`,
+        method: "DELETE",
+      }),
+    }),
   }),
 });
 
-export const { useCreateEventMutation } = eventApiSlice;
+export const {
+  useCreateEventMutation,
+  useGetEventsQuery,
+  useDeleteEventMutation,
+} = eventApiSlice;
