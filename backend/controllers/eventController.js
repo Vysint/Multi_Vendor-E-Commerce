c78f3f0673b65bbff5f26749c10b04a891b252ce
Vincent Kipngetich <vinniebluz@icloud.com>
2024-02-08 22:04:32 +0300

    Added a controller function to get events for a specific shop/seller

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,5 +1,6 @@
-const { request } = require("express");
 const Event = require("../models/eventModel");
+
+const { deleteImageFromCloudinary } = require("../utils/cloudinary");
 
 // @desc   Create an event
 // route   POST /api/v2/events/create-event
@@ -53,6 +54,54 @@ exports.createEvent = async (req, res, next) => {
     } else {
       throw new Error("Failed to create an event, please try again");
     }
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// @desc   Get all events for a given shop
+// route   POST /api/v2/events
+// @access Private
+
+exports.getEvents = async (req, res, next) => {
+  try {
+    const events = await Event.find({ shop: req.seller._id }).sort(
+      "-createdAt"
+    );
+    res.status(200).json(events);
+  } catch (err) {
+    return next(err);
+  }
+};
+// @desc   Delete an event for a given shop
+// route   POST /api/v2/events/:id
+// @access Private
+
+exports.deleteEvent = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const event = await Event.findById(id);
+
+    if (!event) {
+      res.status(404);
+      throw new Error("Event not Found!");
+    }
+    // Check if shop is correct
+    if (event.shop.toString() !== req.seller.id) {
+      res.status(401);
+      throw new Error("User not authorized!");
+    }
+
+    // Delete images in cloudinary for the event
+    await Promise.all(
+      event.images.map(async (image) => {
+        await deleteImageFromCloudinary(image.public_id);
+      })
+    );
+
+    // Delete the event from the database
+    await event.deleteOne();
+    res.status(200).json({ message: "Event deleted successfully." });
   } catch (err) {
     return next(err);
   }
