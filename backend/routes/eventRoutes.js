bf6d3e0b50d96c983fad84d842fe0fa6e4432157
Vincent Kipngetich <vinniebluz@icloud.com>
2024-02-08 22:08:43 +0300

    Added a route to delete an event for a specific shop/seller

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
-const { createEvent, getEvents } = require("../controllers/eventController");
+const {
+  createEvent,
+  getEvents,
+  deleteEvent,
+} = require("../controllers/eventController");
 const protectShop = require("../middlewares/shopMiddleware");
 
 router.post("/create-event", protectShop, createEvent);
 
 router.get("/", protectShop, getEvents);
+
+router.delete("/:id", protectShop, deleteEvent);
 
 module.exports = router;
