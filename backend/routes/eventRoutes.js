2964019427e245f370bff1b449c6c8f46c153402
Vincent Kipngetich <vinniebluz@icloud.com>
2024-02-08 23:15:26 +0300

    Added delete events route for a seller

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const {
   createEvent,
-  getEvents,
   deleteEvent,
+  getEvents,
 } = require("../controllers/eventController");
 const protectShop = require("../middlewares/shopMiddleware");
 
